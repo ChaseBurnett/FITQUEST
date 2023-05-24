bd3517810e42511f56790d9cfca29fb19643c175
ChaseBurnett <chaseburnett@me.com>
2023-05-23 19:56:59 -0500

    Created Method to Create, Delete, Update, and GetById for User.

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace FITQUEST.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+
+        public string userName { get; set; }
+
+        public string email { get; set; }
+
+        public string imgUrl { get; set; }
+    }
+}
---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using FITQUEST.Models;
+
+namespace FITQUEST.Repositories
+{
+    public interface IUserRepository
+    {
+        User AddUser(User userDetails);
+
+        User GetById(int id);
+
+        void DeleteUser(int id);
+
+        void UpdateUser(User userDetails);
+    }
+}
